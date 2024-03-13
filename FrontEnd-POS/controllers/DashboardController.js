31bdc6cb611ef90a391091b8feda5d726664748d
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-13 20:17:42 +0530

    DashboardController.js Refactored

--- FrontEnd-POS/controllers/DashboardController.js
+++ FrontEnd-POS/controllers/DashboardController.js
@@ -5,50 +5,50 @@
 
 let baseUrl = "http://localhost:8080/spring_pos/";
 
-$("#txtCustomerCount").val("00");
+/**
+ * Customer Count
+ **/
 $.ajax({
     url: baseUrl + "customer/CustomerCount",
     method: "GET",
     contentType: "application/json",
     dataType: "json",
     success: function (resp) {
-        let num = resp.count;
-        $("#txtCustomerCount").text(num);
-
+        $("#txtCustomerCount").text(resp.count);
     },
-    error: function (ob, statusText, error) {
-
+    error: function (error) {
+        console.log("Customer Count Error : ", error);
     }
 });
 
-$("#txtItemsCount").val("00");
+/**
+ * Item Count
+ **/
 $.ajax({
     url: baseUrl + "item/itemCount",
     method: "GET",
     contentType: "application/json",
     dataType: "json",
     success: function (resp) {
-        let num = resp.count;
-        $("#txtItemsCount").text(num);
-
+        $("#txtItemsCount").text(resp.count);
     },
-    error: function (ob, statusText, error) {
-
+    error: function (error) {
+        console.log("Item Count Error : ", error);
     }
 });
 
-$("#txtOrderCount").val("00");
+/**
+ * Orders Count
+ **/
 $.ajax({
     url: baseUrl + "orders/ordersCount",
     method: "GET",
     contentType: "application/json",
     dataType: "json",
     success: function (resp) {
-        let num = resp.count;
-        $("#txtOrderCount").text(num);
-
+        $("#txtOrderCount").text(resp.count);
     },
-    error: function (ob, statusText, error) {
-
+    error: function (error) {
+        console.log("Orders Count Error : ", error);
     }
 });
